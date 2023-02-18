dd20c45c9782a43ea1e373a68db2122f8abc5557
MichalTK0 <michalkida2@gmail.com>
2023-02-18 13:07:31 +0000

    Update media page, add spotify page

--- pages/media.py
+++ pages/media.py
@@ -1,26 +1,27 @@
 # Media Controls
 from adafruit_hid.consumer_control_code import ConsumerControlCode
+from adafruit_hid.keycode import Keycode
 
 page = {
     'name' : "Media",
     'macros' : [
         # COLOR    LABEL    KEY SEQUENCE
         # 1st row ----------
-        (0x000000, '', []),
-        (0x000000, '', []),
-        (0x000000, '', []),
-        # 2nd row ----------
-        (0x000000, '', []),
-        (0x000000, '', []),
-        (0x000000, '', []),
-        # 3rd row ----------
         (0x000020, 'Vol-', [[ConsumerControlCode.VOLUME_DECREMENT]]),
         (0x200000, 'Mute', [[ConsumerControlCode.MUTE]]),
         (0x000020, 'Vol+', [[ConsumerControlCode.VOLUME_INCREMENT]]),
+        # 2nd row ----------
+        (0x000000, 'Min', [45*[ConsumerControlCode.VOLUME_DECREMENT]]),
+        (0x000000, 'Lock', [Keycode.WINDOWS, Keycode.L]),
+        (0x000000, 'Max', [50*[ConsumerControlCode.VOLUME_INCREMENT]]),
+        # 3rd row ----------
+        (0x000000, '<', [Keycode.LEFT_ARROW]),
+        (0x000000, 'Seek', []),
+        (0x000000, '>', [Keycode.RIGHT_ARROW]),
         # 4th row ----------
-        (0x202000, '<<', [[ConsumerControlCode.SCAN_PREVIOUS_TRACK]]),
+        (0x202000, '|<', [[ConsumerControlCode.SCAN_PREVIOUS_TRACK]]),
         (0x002000, 'Play/Pause', [[ConsumerControlCode.PLAY_PAUSE]]),
-        (0x202000, '>>', [[ConsumerControlCode.SCAN_NEXT_TRACK]]),
+        (0x202000, '>|', [[ConsumerControlCode.SCAN_NEXT_TRACK]]),
         # Encoder button ---
         (0x000000, '', [])
     ]
---
+++ pages/spotify.py
@@ -0,0 +1,34 @@
+# Spotify Controls
+from adafruit_hid.consumer_control_code import ConsumerControlCode
+from adafruit_hid.keycode import Keycode
+
+"""
+Spotify has different volume control. Thats about it.
+If I can add current song stats on the macropad here one day,
+that'd be pretty neat.
+"""
+
+page = {
+    'name' : "Spotify",
+    'macros' : [
+        # COLOR    LABEL    KEY SEQUENCE
+        # 1st row ----------
+        (0x000000, '', []),
+        (0x000000, '', []),
+        (0x000000, '', []),
+        # 2nd row ----------
+        (0x000020, 'Vol-', [Keycode.CONTROL, Keycode.DOWN_ARROW]),
+        (0x200000, 'Mute', [[ConsumerControlCode.MUTE]]),
+        (0x000020, 'Vol+', [Keycode.CONTROL, Keycode.UP_ARROW]),
+        # 3rd row ----------
+        (0x000000, '<', [Keycode.CONTROL, Keycode.SHIFT, Keycode.LEFT_ARROW]),
+        (0x000000, 'Seek', []),
+        (0x000000, '>', [Keycode.CONTROL, Keycode.SHIFT, Keycode.RIGHT_ARROW]),
+        # 4th row ----------
+        (0x202000, '|<', [[ConsumerControlCode.SCAN_PREVIOUS_TRACK]]),
+        (0x002000, 'Play/Pause', [[ConsumerControlCode.PLAY_PAUSE]]),
+        (0x202000, '>|', [[ConsumerControlCode.SCAN_NEXT_TRACK]]),
+        # Encoder button ---
+        (0x000000, '', [])
+    ]
+}
